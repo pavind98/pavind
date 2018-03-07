d51d177397b41139f6ab1987c3ddaef66415dd00
pavind98 <30829431+pavind98@users.noreply.github.com>
2018-03-07 16:20:40 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,23 @@
+    #include<stdio.h>
+     
+        void main()
+        {
+        int d;
+     
+            printf("enter the day");
+            scanf("%d",&d);
+            if(d==0)
+            {
+                printf("yes it is a holiday");
+            }
+            else if(d==1)
+     
+            {
+                printf("holiday");
+     
+            }
+            else
+            {
+                printf("not holiday");
+            }
+        }
